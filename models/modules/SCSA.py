9cf1e6ba84b8cedba7ad389fc3e1599e616a3a83
wudixiaozhu123 <1113925383@qq.com>
2024-12-09 23:23:29 +0800

    value

---
+++ models/modules/SCSA.py
@@ -0,0 +1,262 @@
+import typing as t
+import torch
+import torch.nn as nn
+from einops import rearrange
+from mmengine.model import BaseModule
+ 
+__all__ = ['SCSA', 'C2f_SCSA1', 'C2f_SCSA2']
+ 
+class SCSA(BaseModule):
+ 
+    def __init__(
+            self,
+            dim: int,
+            head_num: int = 4,
+            window_size: int = 7,
+            group_kernel_sizes: t.List[int] = [3, 5, 7, 9],
+            qkv_bias: bool = False,
+            fuse_bn: bool = False,
+            norm_cfg: t.Dict = dict(type='BN'),
+            act_cfg: t.Dict = dict(type='ReLU'),
+            down_sample_mode: str = 'avg_pool',
+            attn_drop_ratio: float = 0.,
+            gate_layer: str = 'sigmoid',
+    ):
+        super(SCSA, self).__init__()
+        self.dim = dim
+        head_num = dim // 64
+        if head_num == 0:
+            head_num = 1
+        self.head_num = head_num
+        self.head_dim = dim // head_num
+        self.scaler = self.head_dim ** -0.5
+        self.group_kernel_sizes = group_kernel_sizes
+        self.window_size = window_size
+        self.qkv_bias = qkv_bias
+        self.fuse_bn = fuse_bn
+        self.down_sample_mode = down_sample_mode
+ 
+        assert self.dim // 4, 'The dimension of input feature should be divisible by 4.'
+        self.group_chans = group_chans = self.dim // 4
+ 
+        self.local_dwc = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[0],
+                                   padding=group_kernel_sizes[0] // 2, groups=group_chans)
+        self.global_dwc_s = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[1],
+                                      padding=group_kernel_sizes[1] // 2, groups=group_chans)
+        self.global_dwc_m = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[2],
+                                      padding=group_kernel_sizes[2] // 2, groups=group_chans)
+        self.global_dwc_l = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[3],
+                                      padding=group_kernel_sizes[3] // 2, groups=group_chans)
+        self.sa_gate = nn.Softmax(dim=2) if gate_layer == 'softmax' else nn.Sigmoid()
+        self.norm_h = nn.GroupNorm(4, dim)
+        self.norm_w = nn.GroupNorm(4, dim)
+ 
+        self.conv_d = nn.Identity()
+        self.norm = nn.GroupNorm(1, dim)
+        self.q = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        self.k = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        self.v = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        self.attn_drop = nn.Dropout(attn_drop_ratio)
+        self.ca_gate = nn.Softmax(dim=1) if gate_layer == 'softmax' else nn.Sigmoid()
+ 
+        if window_size == -1:
+            self.down_func = nn.AdaptiveAvgPool2d((1, 1))
+        else:
+            if down_sample_mode == 'recombination':
+                self.down_func = self.space_to_chans
+                # dimensionality reduction
+                self.conv_d = nn.Conv2d(in_channels=dim * window_size ** 2, out_channels=dim, kernel_size=1, bias=False)
+            elif down_sample_mode == 'avg_pool':
+                self.down_func = nn.AvgPool2d(kernel_size=(window_size, window_size), stride=window_size)
+            elif down_sample_mode == 'max_pool':
+                self.down_func = nn.MaxPool2d(kernel_size=(window_size, window_size), stride=window_size)
+ 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        The dim of x is (B, C, H, W)
+        """
+        # Spatial attention priority calculation
+        b, c, h_, w_ = x.size()
+        # (B, C, H)
+        x_h = x.mean(dim=3)
+        l_x_h, g_x_h_s, g_x_h_m, g_x_h_l = torch.split(x_h, self.group_chans, dim=1)
+        # (B, C, W)
+        x_w = x.mean(dim=2)
+        l_x_w, g_x_w_s, g_x_w_m, g_x_w_l = torch.split(x_w, self.group_chans, dim=1)
+ 
+        x_h_attn = self.sa_gate(self.norm_h(torch.cat((
+            self.local_dwc(l_x_h),
+            self.global_dwc_s(g_x_h_s),
+            self.global_dwc_m(g_x_h_m),
+            self.global_dwc_l(g_x_h_l),
+        ), dim=1)))
+        x_h_attn = x_h_attn.view(b, c, h_, 1)
+ 
+        x_w_attn = self.sa_gate(self.norm_w(torch.cat((
+            self.local_dwc(l_x_w),
+            self.global_dwc_s(g_x_w_s),
+            self.global_dwc_m(g_x_w_m),
+            self.global_dwc_l(g_x_w_l)
+        ), dim=1)))
+        x_w_attn = x_w_attn.view(b, c, 1, w_)
+ 
+        x = x * x_h_attn * x_w_attn
+ 
+        # Channel attention based on self attention
+        # reduce calculations
+        y = self.down_func(x)
+        y = self.conv_d(y)
+        _, _, h_, w_ = y.size()
+ 
+        # normalization first, then reshape -> (B, H, W, C) -> (B, C, H * W) and generate q, k and v
+        y = self.norm(y)
+        q = self.q(y)
+        k = self.k(y)
+        v = self.v(y)
+        # (B, C, H, W) -> (B, head_num, head_dim, N)
+        q = rearrange(q, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
+                      head_dim=int(self.head_dim))
+        k = rearrange(k, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
+                      head_dim=int(self.head_dim))
+        v = rearrange(v, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
+                      head_dim=int(self.head_dim))
+ 
+        # (B, head_num, head_dim, head_dim)
+        attn = q @ k.transpose(-2, -1) * self.scaler
+        attn = self.attn_drop(attn.softmax(dim=-1))
+        # (B, head_num, head_dim, N)
+        attn = attn @ v
+        # (B, C, H_, W_)
+        attn = rearrange(attn, 'b head_num head_dim (h w) -> b (head_num head_dim) h w', h=int(h_), w=int(w_))
+        # (B, C, 1, 1)
+        attn = attn.mean((2, 3), keepdim=True)
+        attn = self.ca_gate(attn)
+        return attn * x
+ 
+ 
+ 
+ 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+ 
+ 
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+ 
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+ 
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+ 
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+ 
+ 
+class Bottleneck_1(nn.Module):
+    """Standard bottleneck."""
+ 
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.att = SCSA(c_, c_ // 64)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+ 
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.att(self.cv1(x)))  # 使用add进行特征提取的时候利用SCSA机制.
+ 
+ 
+class Bottleneck_2(nn.Module):
+    """Standard bottleneck."""
+ 
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.att = SCSA(c_, c_ // 64)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+ 
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.cv2(self.att(self.cv1(x))) if self.add else self.cv2(self.cv1(x))  # 进行特征提取的时候利用SCSA机制,特征融合时不用SCSA机制.
+ 
+ 
+ 
+class C2f_SCSA1(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+ 
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck_1(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+ 
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+class C2f_SCSA2(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+ 
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck_2(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+ 
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+ 
+if __name__ == '__main__':
+    x = torch.ones(8, 128, 32, 32)
+    channels = x.shape[1]
+    model = C2f_SCSA1(channels, channels, 1,True)
+    output = model(x)
+    print(output.shape)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+"cuda device, i.e. 0 or 0,1,2,3 or cpu")
+    parser.add_argument("--multi-scale", action="store_
